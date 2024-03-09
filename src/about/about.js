e0617bf4799b592c71767a2b7075bb319746c1e3
Yifei Zeng <yzeng33@my.bcit.ca>
2024-03-09 15:21:02 -0800

    Add about page

---
+++ src/about/about.js
@@ -0,0 +1,12 @@
+import './about.css'
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default about
---
+++ src/about/aboutPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import About from "./about.js";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <About />
+    </React.StrictMode>
+);
