6ca690357aae35dc16ae94a85fe3b852760fea0d
Yifei Zeng <yzeng33@my.bcit.ca>
2024-03-09 16:25:06 -0800

    Add contact us section

--- src/scenes/about/index.jsx
+++ src/scenes/about/index.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import './about.css';
+import Contact from './contact'
 
 const About = () => {
     return (
-        <div className="contact-page-wrapper">
-            <h1 className="primary-heading">Have Question In Mind?</h1>
-            <h1 className="primary-heading">Let Us Help You</h1>
-            <div className="contact-form-container">
-                <input type="text" placeholder="yourmail@gmail.com" />
-                <button className="secondary-button">Submit</button>
-            </div>
+        <div className="App">
+            {/* <Home />
+            <About />
+            <Work />
+            <Testimonial /> */}
+            <Contact />
+            {/* <Footer /> */}
         </div>
     )
 }
