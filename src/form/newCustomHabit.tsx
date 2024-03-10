d98e2d0a8aabb71133400e175097200d01c7ec8d
Inez Yoon <inez3cs@gmail.com>
2024-03-10 01:40:21 -0800

    ADD alert and new design

--- src/form/newCustomHabit.tsx
+++ src/form/newCustomHabit.tsx
@@ -21,11 +21,7 @@ const sampleColours = [
   "#FFDAB9"  // Peach Puff
 ];
 
-type Props = {
-  onAddHabit: Function;
-}
-
-const Uncontrolled: React.FC<Props> = (props) => {
+const Uncontrolled: React.FC = () => {
   const habitIconRef = useRef<HTMLInputElement>(null);
   const habitNameRef = useRef<HTMLInputElement>(null);
   const habitDescriptionRef = useRef<HTMLInputElement>(null);
@@ -49,7 +45,7 @@ const Uncontrolled: React.FC<Props> = (props) => {
     if (habitStartDateRef.current) habitStartDateRef.current.value = '';
     if (habitEndDateRef.current) habitEndDateRef.current.value = '';
   };
-
+  
 
   const handleIconClick = (icon: string) => {
     setSelectedIcon(icon);
@@ -66,8 +62,10 @@ const Uncontrolled: React.FC<Props> = (props) => {
   };
 
   function handleHabitTypeClick(type: string) {
-    setHabitType(type);
+    setHabitType(prevType => prevType === type ? "" : type);
   }
+  
+  
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -81,242 +79,226 @@ const Uncontrolled: React.FC<Props> = (props) => {
     console.log("Habit term starts :", habitStartDateRef.current?.value);
     console.log("Habit term ends :", habitEndDateRef.current?.value);
 
-    props.onAddHabit(habitNameRef.current?.value,
-      habitDescriptionRef.current?.value,
-      habitIconRef.current?.value,
-      selectedColour,
-      habitGoalRef.current?.value,
-      habitGoalPeriodRef.current?.value,
-      habitStartDateRef.current?.value,
-      habitEndDateRef.current?.value);
-
-    // onAddHabit();
-    // HabitDataService.create({
-    //   "habit_name": `${habitNameRef.current?.value}`,
-    //   "habit_desc": `${habitDescriptionRef.current?.value}`,
-    //   "icon": `${habitIconRef.current?.value}`,
-    //   "color": `${selectedColour}`,
-    //   "habit_type": `${habitGoalRef.current?.value}`,
-    //   "goal": `${habitGoalPeriodRef.current?.value}`,
-    //   "start_date": `${habitStartDateRef.current?.value}`,
-    //   "end_date": `${habitEndDateRef.current?.value}`
-    // })
-    //   .then(response => { console.log(response.data) });
+    HabitDataService.create({
+      "habit_name": `${habitNameRef.current?.value}`,
+      "habit_desc": `${habitDescriptionRef.current?.value}`,
+      "icon": `${habitIconRef.current?.value}`,
+      "color": `${selectedColour}`,
+      "habit_type": `${habitGoalRef.current?.value}`,
+      "goal": `${habitGoalPeriodRef.current?.value}`,
+      "start_date": `${habitStartDateRef.current?.value}`,
+      "end_date": `${habitEndDateRef.current?.value}`
+    })
+      .then(response => { console.log(response.data) });
 
     console.log("Submitted!");
-    alert("Habit added successfully!");
+    window.alert("Form submitted successfully!");
   }
 
   return (
     <section id="form" className="bg-gray-20">
-      <form onSubmit={handleSubmit} className="max-w-lg mx-auto p-4 bg-gray-20 p-20 text-xl lg:text-6xl font-light leading-tight tracking-tight text-gray-500">
-        <div className="mb-4 text-center ">
-          <h2> Make A New Habit </h2>
-          <label className="block mb-2">
-            <br></br>
-            <p>New Habit Name:</p>
-            <input
-              ref={habitNameRef}
-              type="text"
-              className="block w-full px-4 py-2 rounded border-gray-300 focus:border-blue-500 focus:outline-none"
-            />
-          </label>
+    <form onSubmit={handleSubmit} className="max-w-lg mx-auto p-4 bg-gray-20 p-20 text-xl lg:text-6xl font-light leading-tight tracking-tight text-gray-500">
+      <div className="mb-4 text-center ">
+        <h2> Make A New Habit </h2>
         </div>
-
         <div className="mb-4">
-          <label className="block mb-2 text-center">
-            <p>Description:</p>
-            <input
-              ref={habitDescriptionRef}
-              type="text"
-              className="block w-full px-4 py-2 rounded border-gray-300 focus:border-blue-500 focus:outline-none"
-            />
-          </label>
-        </div>
+        <label className="block mb-2">
+          <br></br>
+          <p>New Habit Name:</p>
+          <input
+            ref={habitNameRef}
+            type="text"
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+        </label>
+      </div>
 
-        <div className="mb-4">
-          <label className="block mb-2 text-center">
-            <p>Choose Icon:</p>
-            <div className="flex flex-wrap gap-2 justify-center text-center">
-              {iconList.map(({ icon, label }) => (
-                <button
-                  type='button'
-                  ref={habitIconRef}
-                  key={label}
-                  className={`p-2 rounded-full focus:outline-none ${selectedIcon === icon ? "text-4xl bg-green-500 text-white" : ""
-                    }`}
-                  onClick={() => handleIconClick(icon)}
-                >
-                  <i className="material-icons text-2xl">{icon}</i> {/* Adjust text size here */}
-                </button>
-              ))}
-            </div>
-          </label>
-        </div>
+      <div className="mb-4">
+        <label className="block mb-2">
+          <p>Description:</p>
+          <input
+            ref={habitDescriptionRef}
+            type="text"
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+        </label>
+      </div>
 
-
-        <div className="mb-4">
-          <label className="block mb-2 text-center">
-            <p>Colour:</p>
-            <input
-              ref={habitColourRef}
-              type="color"
-              value={selectedColour}
-              className="hidden"
-              onChange={(e) => setSelectedColour(e.target.value)}
-            />
-            <div className="flex items-center space-x-2 justify-center">
-              {sampleColours.map((colour, index) => (
-                <button
-                  type='button'
-                  key={index}
-                  className="w-10 h-10 rounded-full border border-gray-300 focus:outline-none flex items-center justify-center"
-                  style={{ backgroundColor: colour }}
-                  onClick={() => handleColourChange(colour)}
-                >
-
-                </button>
-              ))}
-
-              {/* test colour picker */}
-              <button className="w-10 h-10 rounded-full border border-gray-300 focus:outline-none flex items-center justify-center"
-                onClick={() => {
-                  if (habitColourRef.current) {
-                    habitColourRef.current.click();
-                  }
-                }}> <input type="color" className="p-1 h-10 w-14 block bg-white border border-gray-200 cursor-pointer rounded-lg disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700" id="hs-color-input" value="#2563eb" title="Choose your color"></input></button>
-            </div>
-          </label>
-        </div>
-
-        <div className="mb-4">
-          <label className="block mb-2 text-center">
-            <p>Habit Type:</p>
-            <div className="flex justify-center">
+      <div className="mb-4">
+        <label className="block mb-2">
+          <p>Choose Icon:</p>
+          <div className="flex flex-wrap gap-2">
+            {iconList.map(({ icon, label }) => (
               <button
                 type='button'
-                className={`rounded-md bg-secondary-500 px-5 py-1 hover:bg-primary-500 hover:text-white no-underline text-gray-500 ${habitType === "quit"
-                  ? "bg-white-500 text-white"
-                  : "bg-white-300 text-gray-700"
+                ref={habitIconRef}
+                key={label}
+                className={`tw-10 h-15 rounded-full border border-gray-300 focus:outline-none flex items-center justify-center ${selectedIcon === icon ? "bg-secondary-500 text-white" : ""
                   }`}
-                onClick={() => handleHabitTypeClick("quit")}
+                onClick={() => handleIconClick(icon)}
               >
-                Quit
+                <i className="material-icons text-5xl">{icon}</i> {/* Adjust text size here */}
               </button>
-              <button
-                type='button'
-                className={`rounded-md bg-secondary-500 px-5 py-1 hover:bg-primary-500 hover:text-white no-underline text-gray-500" ${habitType === "build"
-                  ? "bg-white-500 text-white"
-                  : "bg-white-300 text-gray-700"
-                  }`}
-                onClick={() => handleHabitTypeClick("build")}
-              >
-                Build
-              </button>
-            </div>
-          </label>
+            ))}
+          </div>
+        </label>
+      </div>
+
+
+      <div className="mb-4">
+      <label className="block mb-2">
+        <p>Colour:</p>
+        <input
+          ref={habitColourRef}
+          type="color"
+          value={selectedColour}
+          className="hidden"
+          onChange={(e) => setSelectedColour(e.target.value)}
+        />
+        <div className="flex items-center space-x-2 justify-center">
+          {sampleColours.map((colour, index) => (
+            <button
+              type='button'
+              key={index}
+              className="w-10 h-10 rounded-full border border-gray-300 focus:outline-none flex items-center justify-center"
+              style={{ backgroundColor: colour }}
+              onClick={() => handleColourChange(colour)}
+            >
+            </button>
+          ))}
+
+          {/* test colour picker */}
+      <button className="w-10 h-10 rounded-full border border-gray-300 focus:outline-none flex items-center justify-center" 
+      onClick={() => {
+              if (habitColourRef.current) {
+                habitColourRef.current.click(); 
+              }
+            }}> <input type="color" className="w-10 h-10 rounded-full focus:outline-none flex items-center justify-center" id="hs-color-input" value="#F2F3EE" title="Choose your color"></input></button>
         </div>
+      </label>
+    </div>
+
+    <div className="mb-4">
+    <label className="block mb-2">
+      <p>Habit Type:</p>
+      <div className="flex">
+        <button
+          type='button'
+          className={`border-none m-1 rounded-md ${habitType === "quit" ? "bg-green-500 text-white" : "bg-secondary-500 text-gray-500"} px-5 py-1 hover:bg-primary-500 hover:text-white no-underline`}
+          onClick={() => handleHabitTypeClick("quit")}
+        >
+          Quit
+        </button>
+        <button
+          type='button'
+          className={`border-none m-1 rounded-md ${habitType === "build" ? "bg-green-500 text-white" : "bg-secondary-500 text-gray-500"} px-5 py-1 hover:bg-primary-500 hover:text-white no-underline`}
+          onClick={() => handleHabitTypeClick("build")}
+        >
+          Build
+        </button>
+      </div>
+    </label>
+  </div>
 
 
-        <div className="mb-4">
-          <label className="block mb-2 text-center">
-            <p>Goal:</p>
-            <input
-              ref={habitGoalRef}
-              type="text"
-              placeholder="e.g., 100 steps"
-              className="block w-full px-4 py-2 rounded border-gray-300 focus:border-blue-500 focus:outline-none"
-            />{" "}
-            per
-            <br />
-            <input
-              type="radio"
-              name="goalPeriod"
-              value="day"
-              ref={habitGoalPeriodRef}
-              className="mx-2"
-            />
-            <label className="mr-4">day</label>
-            <input
-              type="radio"
-              name="goalPeriod"
-              value="week"
-              ref={habitGoalPeriodRef}
-              className="mx-2"
-            />
-            <label className="mr-4">week</label>
-            <input
-              type="radio"
-              name="goalPeriod"
-              value="month"
-              ref={habitGoalPeriodRef}
-              className="mx-2"
-            />
-            <label>month</label>
-          </label>
-        </div>
+      <div className="mb-4">
+        <label className="block mb-2">
+          <p>Goal:</p>
+          <input
+            ref={habitGoalRef}
+            type="text"
+            placeholder="e.g., 100 steps "
+            className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />{" "}
+          per
+          
+          <input
+            type="radio"
+            name="goalPeriod"
+            value="day"
+            ref={habitGoalPeriodRef}
+            className="mx-2"
+          />
+          <label className="mr-4">day</label>
+          <input
+            type="radio"
+            name="goalPeriod"
+            value="week"
+            ref={habitGoalPeriodRef}
+            className="mx-2"
+          />
+          <label className="mr-4">week</label>
+          <input
+            type="radio"
+            name="goalPeriod"
+            value="month"
+            ref={habitGoalPeriodRef}
+            className="mx-2"
+          />
+          <label>month</label>
+        </label>
+      </div>
 
-        <div className="mb-4">
-          <label className="block mb-2 text-center">
-            <p>Habit Term:</p>
-            <div className="flex flex-col">
-              <div className="grid grid-cols-2 gap-4 mb-4">
-                <div>
-                  <label className="block mb-1">Start Date:</label>
-                  <input
-                    type="date"
-                    ref={habitStartDateRef}
-                    className="block w-full px-4 py-2 rounded border-gray-300 focus:border-blue-500 focus:outline-none"
-                  />
-                </div>
-                <div>
-                  <label className="block mb-1">End Date:</label>
-                  <input
-                    type="date"
-                    ref={habitEndDateRef}
-                    className="block w-full px-4 py-2 rounded border-gray-300 focus:border-blue-500 focus:outline-none"
-                  />
-                </div>
-              </div>
-              <div className="flex justify-center">
+      <div className="mb-4">
+        <label className="block mb-2">
+          <p>Habit Term:</p>
+          <div className="flex flex-col">
+            <div className="grid grid-cols-2 gap-4 mb-4">
+              <div>
+                <label className="block mb-1 text-lg">Start Date</label>
                 <input
-                  type="checkbox"
-                  id="noEndDate"
-                  onChange={(e) => {
-                    if (e.target.checked) {
-                      habitEndDateRef.current.disabled = true;
-                      habitEndDateRef.current.value = '';
-                    } else {
-                      habitEndDateRef.current.disabled = false;
-                    }
-                  }}
+                  type="date"
+                  ref={habitStartDateRef}
+                  className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
-                <label htmlFor="noEndDate" className="ml-2">No End Date</label>
+              </div>
+              <br></br>
+              <div>
+                <label className="block mb-1 text-lg">End Date</label>
+                <input
+                  type="date"
+                  ref={habitEndDateRef}
+                  className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+                <input
+                type="checkbox"
+                id="noEndDate"
+                onChange={(e) => {
+                  if (e.target.checked) {
+                    habitEndDateRef.current.disabled = true;
+                    habitEndDateRef.current.value = '';
+                  } else {
+                    habitEndDateRef.current.disabled = false;
+                  }
+                }}
+              />
+              <label htmlFor="noEndDate" className="ml-2 text-lg">No End Date</label>
               </div>
             </div>
-          </label>
-        </div>
+          </div>
+        </label>
+      </div>
 
-        <div className="mb-4 flex justify-center items-center">
-          <button
-            type="submit"
-            className="rounded-md bg-secondary-500 px-5 py-1 hover:bg-primary-500 hover:text-white no-underline text-gray-500"
-          >
-            Submit
-          </button>
+      <div className="mb-4 flex items-center">
+  <button
+    type="submit"
+    className="border-none rounded-md bg-secondary-500 px-5 py-1 hover:bg-primary-500 hover:text-white no-underline text-gray-500"
+  >
+    Submit
+  </button>
 
-          <button
-            type="button"
-            onClick={clearForm}
-            className="rounded-md bg-secondary-500 px-5 py-1 hover:bg-primary-500 hover:text-white no-underline text-gray-500"
-          >
-            Clear
-          </button>
-        </div>
+  <button
+  type="button"
+  onClick={clearForm}
+  className="border-none m-1 rounded-md bg-secondary-500 px-5 py-1 hover:bg-primary-500 hover:text-white no-underline text-gray-500"
+>
+  Clear
+</button>
+</div>
 
-      </form>
+    </form>
     </section>
-
+    
   );
 };
 
