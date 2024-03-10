6eefbae4bc1ee5c7e3562d360b0b74014ce4b4be
TinLeaves <tinleaves.0@gmail.com>
2024-03-09 19:44:04 -0800

    fixed foreign key column issue

--- backend/src/main/java/com/habitTracker/backend/HabitRepository.java
+++ backend/src/main/java/com/habitTracker/backend/HabitRepository.java
@@ -2,8 +2,6 @@ package com.habitTracker.backend;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.habitTracker.backend.User;
-
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
