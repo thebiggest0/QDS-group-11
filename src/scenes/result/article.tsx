b86ac316430a9178145ef70f6f1fe6df2b749e36
Atsuko Uemura <atsukof.kt@gmail.com>
2024-03-10 01:24:05 -0800

    Retry commit

--- src/scenes/result/article.tsx
+++ src/scenes/result/article.tsx
@@ -25,7 +25,7 @@ const Article = ({
     return (
         <div className="resultCard p-10 text-left w-[800px] bg-gray-20 rounded-xl mb-5">
             <h2 className="leading-tight tracking-tight text-primary-500">{icon}{name}</h2>
-            <p className="text-xl lg:text-6xl font-light leading-tight tracking-tight text-gray-500">Goal: {type} per {goal}</p>
+            <p className="text-xl lg:text-6xl font-light leading-tight tracking-tight text-gray-500">Goal: {type}</p>
             <p className="text-xl lg:text-6xl font-light leading-tight tracking-tight text-gray-500">{desc}</p>            
             <button
                 className="rounded-md bg-secondary-500 px-10 py-2 hover:bg-primary-500 hover:text-white no-underline text-white border-none"
