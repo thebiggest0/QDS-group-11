007934724b642d2d8a3b700eef054e4c7562fed4
Yifei Zeng <yzeng33@my.bcit.ca>
2024-03-09 19:27:21 -0800

    Change footer color

--- src/scenes/about/footer.jsx
+++ src/scenes/about/footer.jsx
@@ -7,7 +7,7 @@ import { FaFacebookF } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <div className="footer-wrapper">
+        <div className="footer-wrapper bg-primary-100">
             <div className="footer-section-one">
                 <div className="footer-logo-container">
                     <img src={Logo} alt="" />
