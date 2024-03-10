55d7195f21ddec32ff82dcc1a2fba280199cda7d
Cai Chun Yan <caichunyan17@gmail.com>
2024-03-10 01:55:19 -0800

    Update result spacing

--- src/scenes/result/article.tsx
+++ src/scenes/result/article.tsx
@@ -24,14 +24,14 @@ const Article = ({
 }: Props) => {
     return (
         <div className="resultCard p-10 text-left w-[800px] bg-gray-20 rounded-xl mb-5">
-            <h2 className="leading-tight tracking-tight text-primary-500">{icon}{name}</h2>
-            <p className="text-xl lg:text-6xl font-light leading-tight tracking-tight text-gray-500">Goal: {type}</p>
-            <p className="text-xl lg:text-6xl font-light leading-tight tracking-tight text-gray-500">{desc}</p>            
+            <h2 className="leading-tight tracking-tight text-primary-500">{icon} {name}</h2>
+            <p className="mt-2 text-xl lg:text-6xl font-light leading-tight tracking-tight text-gray-500"><span className="text-primary-500">Goal:</span>{type}</p>
+            <p className="text-xl lg:text-6xl font-light leading-tight tracking-tight text-gray-500"><span className="text-primary-500">Description:</span>{desc}</p>
             <button
-                className="rounded-md bg-secondary-500 px-10 py-2 hover:bg-primary-500 hover:text-white no-underline text-white border-none"
+                className="mt-5 rounded-md bg-secondary-500 px-10 py-2 hover:bg-primary-500 hover:text-white no-underline text-white border-none"
                 onClick={onDelete}
             >
-                Read More
+                Delete
             </button>
         </div>
     );
