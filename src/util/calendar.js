7b34aca21eb088d1a45bbb3f7e8485db73182f32
Cai Chun Yan <caichunyan17@gmail.com>
2024-03-09 21:37:11 -0800

    Fix calendar issue

--- src/util/calendar.js
+++ src/util/calendar.js
@@ -1,36 +1,43 @@
 import dayjs from "dayjs";
 
 export const generateDate = (month = dayjs().month(), year = dayjs().year()) => {
-    const firstDateOfMonth = dayjs().month(month).year(year).startOf('month').startOf('month');
-    const lastDateOfMonth = dayjs().month(month).year(year).endOf('month').endOf('month');
-    const arrayOfDate = []
+    const firstDayOfMonth = dayjs().month(month).year(year).startOf('month');
+    const lastDayOfMonth = dayjs().month(month).year(year).endOf('month');
+    const daysInMonth = lastDayOfMonth.date(); // Get the last date number of the month
+    const arrayOfDate = [];
 
     // create prefix date
-
-    for (let i = 0; i < firstDateOfMonth.day(); i++) {
-        arrayOfDate.push({ currentMonth: false, date: firstDateOfMonth.day(i) })
+    for (let i = 0; i < firstDayOfMonth.day(); i++) {
+        arrayOfDate.unshift({ // use unshift to add to the beginning
+            currentMonth: false,
+            date: firstDayOfMonth.subtract(i + 1, 'day'), // go backwards for prefix days
+            today: false
+        });
     }
 
-    // generate current date
-
-    for (let i = firstDateOfMonth.date(); i <= lastDateOfMonth.date(); i++) {
+    // generate current month date
+    for (let i = 0; i < daysInMonth; i++) {
+        const date = firstDayOfMonth.add(i, 'day'); // iterate through the current month
         arrayOfDate.push({
             currentMonth: true,
-            date: firstDateOfMonth.day(i),
-            today: firstDateOfMonth.day(i).toDate().toDateString() === dayjs().toDate().toDateString()
-        }
-        );
+            date: date,
+            today: date.isSame(dayjs(), 'day') // check if it's today
+        });
     }
 
     // create suffix date
-    const remaining = 42 - arrayOfDate.length;
-
-    for (let i = lastDateOfMonth.date() + 1; i <= lastDateOfMonth.date() + remaining; i++) {
-        arrayOfDate.push({ currentMonth: false, date: lastDateOfMonth.date(i) });
+    const daysToAdd = 42 - arrayOfDate.length; // to make up a total of 6 weeks
+    for (let i = 1; i <= daysToAdd; i++) {
+        arrayOfDate.push({
+            currentMonth: false,
+            date: lastDayOfMonth.add(i, 'day'), // continue with next month's dates
+            today: false
+        });
     }
 
     return arrayOfDate;
-}
+};
+
 
 export const months = [
     "January",
