55b783f01e372eba1b003c3246438aa8900ae8ca
TinLeaves <tinleaves.0@gmail.com>
2024-03-09 22:06:54 -0800

    added delete function to controller

--- backend/src/main/java/com/habitTracker/backend/MainController.java
+++ backend/src/main/java/com/habitTracker/backend/MainController.java
@@ -3,7 +3,9 @@ package com.habitTracker.backend;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -116,5 +118,14 @@ public class MainController {
   public @ResponseBody Iterable<Mood> getAllMoods() { return moodRepository.findAll();
   }
 
+  @DeleteMapping("/habits/{id}")
+  public ResponseEntity<HttpStatus> deleteHabit(@PathVariable("id") long id) {
+    try {
+      habitRepository.deleteById((int) id);
+      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    } catch (Exception e) {
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
 
 }
